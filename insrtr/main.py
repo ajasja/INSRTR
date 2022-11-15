3ee0825cf56c62aa239ff887d93cd71a31df2c60
Ajasja Ljubetic <ajasja.ljubetic@gmail.com>
2022-11-15 17:30:47 +0100

    Added insert functionality

--- insrtr/main.py
+++ insrtr/main.py
@@ -1,1 +1,7 @@
 """Main module."""
+
+
+def insert_sequence(seq, one_based_position, insert):
+    """Inserts an AA sequence into another AA sequence. Uses 1 based indexing"""
+    pos = one_based_position - 1
+    return  seq[:pos] + insert + seq[pos:]
